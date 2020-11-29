98aa83ee5d4db2c6bf751d345df45adf355cb818
Varad Gauthankar <gvarad1601@gmail.com>
2020-11-29 14:22:21 +0530

    Update index.js

--- src/components/GetInTouchSection/index.js
+++ src/components/GetInTouchSection/index.js
@@ -50,7 +50,12 @@ const GetInTouch = () => {
           </Column1>
           <Column2>
             <FormContainer>
-              <form name="contact" method="POST" data-netlify="true">
+              <form
+                name="contact"
+                method="POST"
+                data-netlify="true"
+                enctype="application/x-www-form-urlencoded"
+              >
                 <p>
                   <label>
                     Your Name: <input type="text" name="name" />
@@ -61,15 +66,7 @@ const GetInTouch = () => {
                     Your Email: <input type="email" name="email" />
                   </label>
                 </p>
-                <p>
-                  <label>
-                    Your Role:{" "}
-                    <select name="role[]" multiple>
-                      <option value="leader">Leader</option>
-                      <option value="follower">Follower</option>
-                    </select>
-                  </label>
-                </p>
+
                 <p>
                   <label>
                     Message: <textarea name="message"></textarea>
