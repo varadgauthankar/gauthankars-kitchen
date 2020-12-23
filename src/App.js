c71e044a19ab0600cbda8a9a751c9e607a2ee266
Varad Gauthankar <gvarad1601@gmail.com>
2020-12-23 20:07:01 +0530

    fixed form submit page issue

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/HomePage";
 import MenuePage from "./pages/menue/";
 import Direction from "./pages/Direction";
-import FormSubmitted from "./pages/FormSubmitted";
 import NotFound from "./pages/NotFound";
 
 function App() {
@@ -13,7 +12,6 @@ function App() {
         <Route path="/" component={Home} exact />
         <Route path="/menue" component={MenuePage} exact />
         <Route path="/direction" component={Direction} exact />
-        <Route path="/form-submitted" component={FormSubmitted} exact />
         <Route component={NotFound} />
       </Switch>
     </Router>
--- src/pages/FormSubmitted.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const FormSubmitted = () => {
-  return <div>Form Submited</div>;
-};
-
-export default FormSubmitted;
