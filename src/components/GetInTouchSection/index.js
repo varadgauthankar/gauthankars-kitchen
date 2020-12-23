6ae6ea662f1cde4bd74e38fc2897ecc23347ed38
Varad Gauthankar <gvarad1601@gmail.com>
2020-12-23 14:56:39 +0530

    :'(

--- src/components/GetInTouchSection/index.js
+++ src/components/GetInTouchSection/index.js
@@ -67,7 +67,7 @@ const GetInTouch = () => {
               <form
                 name="contact"
                 method="POST"
-                action="/src/pages/FormSubmitted"
+                action="/src/pages/FormSubmitted.js"
                 data-netlify="true"
               >
                 <input type="hidden" name="form-name" value="contact" />
