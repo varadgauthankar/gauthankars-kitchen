a4d561a2efdfb597994c9da7ae3a8d8d41731d75
Varad Gauthankar <gvarad1601@gmail.com>
2020-12-25 14:59:22 +0530

    Update NotFound.js

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -2,32 +2,62 @@ import React from "react";
 import styled from "styled-components";
 
 const NotFound = () => {
+  const back = "<";
   return (
-    <Container>
-      <Wrap>
-        <Text>404 :(</Text>
-      </Wrap>
-    </Container>
+    <Wrapper>
+      <TextWrapper>
+        <Title>404</Title>
+        <Subtitle>This is not the web page you're looking for</Subtitle>
+        <Button href="/"> {back} Back to home</Button>
+      </TextWrapper>
+    </Wrapper>
   );
 };
 
 export default NotFound;
 
-export const Container = styled.div`
+export const Wrapper = styled.div`
   height: 100vh;
-  width: 100%;
-  background: #212121;
-  padding: 50px;
+  position: relative;
+  background-color: #221f1e;
 `;
 
-export const Wrap = styled.div`
-  margin-left: auto;
-  margin-right: auto;
+export const TextWrapper = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #2b2727;
+  padding: 18px 22px;
+  border-radius: 8px;
+  box-shadow: 6px 6px 12px rgba(255, 31, 31, 0.1);
 `;
 
-export const Text = styled.p`
+export const Title = styled.p`
+  font-size: 28px;
+  color: #ffedc4;
+  font-weight: 800;
+
+  @media only screen and (max-width: 768px) {
+    font-size: 22px;
+  }
+`;
+
+export const Subtitle = styled.p`
+  font-size: 22px;
   color: #fff;
-  font-size: 40px;
 
-  text-align: center;
+  @media only screen and (max-width: 768px) {
+    font-size: 18px;
+  }
+`;
+
+export const Button = styled.a`
+  color: #ff2929;
+  text-decoration: none;
+
+  :hover {
+    color: #ff2929;
+    text-decoration: underline;
+  }
 `;
