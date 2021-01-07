feea816ba78ccdb56c0d62c1439f3142d329dae0
Varad Gauthankar <gvarad1601@gmail.com>
2021-01-07 13:32:57 +0530

    fixing form

--- src/components/GetInTouchSection/index.js
+++ src/components/GetInTouchSection/index.js
@@ -75,7 +75,7 @@ const GetInTouch = () => {
               <form
                 name="contact"
                 method="POST"
-                action="/form-submitted"
+                action="/submitted"
                 data-netlify="true"
               >
                 <input type="hidden" name="form-name" value="contact" />
