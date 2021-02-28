d9a6dfd7277c4aa075d488c37e3426476db468d3
varadgauthankar <gvarad1601@gmail.com>
2021-02-28 23:58:43 +0530

    pls work

--- src-sw.js
+++
@@ -1,3 +0,0 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
-
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
--- workbox-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  "globDirectory": "public/",
-  "globPatterns": [
-    "**/*.{jpg,ico,html,png,json,txt,js}"
-  ],
-  "swDest": "public/sw.js",
-  "swSrc": "src-sw.js"
-};
